d7d1e209f685c16775b8917992df28e28a54be49
hehengsong <hehengsong@58ganji.com>
2019-01-16 19:03:49 +0800

    'hah'

--- src/main/java/studyProdxy.java
+++ src/main/java/studyProdxy.java
@@ -1,6 +1,10 @@
+import org.apache.commons.collections.CollectionUtils;
 import self.service.*;
 
 import java.lang.reflect.Proxy;
+import java.util.*;
+
+import static java.util.Collections.*;
 
 public class studyProdxy {
 
@@ -13,6 +17,18 @@ public class studyProdxy {
 
         liquor proxy = (liquor) Proxy.newProxyInstance(mao.getClass().getClassLoader(), new Class[]{liquor.class},new LiquorProxy(mao));
          proxy.taste();
+
+         Map map = new LinkedHashMap();
+         map.put("1","58");
+        map.put("2","58");
+
+        List<Integer> list = new ArrayList<Integer>(){{
+            add(4);
+            add(3);
+        }};
+        System.out.println(list);
+        sort(list,);
+        System.out.println(list);
     }
 
     public static void printA(){
